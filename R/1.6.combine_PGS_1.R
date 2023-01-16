a3a90d65ad22b4aa37e9604bb6cb800fa335e0b5
buutrg <buutrg@gmail.com>
2023-01-15 19:51:50 -0500

    update AUC

--- R/1.6.combine_PGS_1.R
+++ R/1.6.combine_PGS_1.R
@@ -258,12 +258,12 @@ combine_PGS = function(
 			test_pred = predict(model_prsmix_null, test_data1, type = "prob")[,2]
 			auc_ci = ci.auc(test_data1$trait, test_pred)
 			auc_out = data.frame(method="null_model", auc=auc_ci[2], lowerCI=auc_ci[1], upperCI=auc_ci[3])
-			fwrite(auc_out, paste0(out, "_power.", power_thres, "_pthres.", pval_thres, "_aucNULL.txt"), row.names=F, sep="\t", quote=F)
+			fwrite(auc_out, paste0(out, "_aucNULL.txt"), row.names=F, sep="\t", quote=F)
 			
 			test_pred = predict(model_prsmix_bestPRS, test_data1, type = "prob")[,2]
 			auc_ci = ci.auc(test_data1$trait, test_pred)
 			auc_out = data.frame(method="bestPGS", auc=auc_ci[2], lowerCI=auc_ci[1], upperCI=auc_ci[3])
-			fwrite(auc_out, paste0(out, "_power.", power_thres, "_pthres.", pval_thres, "_aucBestPGS.txt"), row.names=F, sep="\t", quote=F)			
+			fwrite(auc_out, paste0(out, "_aucBestPGS.txt"), row.names=F, sep="\t", quote=F)
 			
 		}
 
