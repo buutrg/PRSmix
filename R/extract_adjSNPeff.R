07ec4c4e39939bae354c7ae49e6b35ecce222a6a
buutrg <buutrg@gmail.com>
2023-02-13 18:09:44 -0500

    fix and optimize extract SNP weights

--- R/extract_adjSNPeff.R
+++ R/extract_adjSNPeff.R
@@ -22,11 +22,15 @@ extract_adjSNPeff = function(
 
         print(snp_eff_files)
         score_file = snp_eff_files_list[snp_eff_files]
-        dd = fread(score_file)
+        dd = fread(score_file, nrow=1)
         
         idx = which(colnames(dd) %in% mixing_weight[,1])
         if (length(idx)==0) next()
         
+        cmd = paste0("awk '{print $1,$2,$3,", paste(idx, collapse=",$"), "}' ", score_file)
+        dd = fread(cmd = cmd)
+        
+        idx = which(colnames(dd) %in% mixing_weight[,1])
         idx2 = match(colnames(dd)[idx], mixing_weight[,1])
         snp_eff_vec = as.matrix(dd[,idx]) %*% as.vector(mixing_weight[idx2,2])
         snp_eff_vec_tmp = data.frame(dd[,1:3], beta=snp_eff_vec)
