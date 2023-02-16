be602f87ba3633874b7ea84616bee3a3770f90de
Buu Broad <trminhthanhbuu04@gmail.com>
2023-02-16 09:43:34 -0500

    update plink download

--- R/compute_PRS.R
+++ R/compute_PRS.R
@@ -2,7 +2,6 @@
 #'
 #' This function estimate PRS with a reference scores
 #'
-#' @param plink_exe The weight file.
 #' @param geno Genotype file in plink format (bed/bim/fam).
 #' @param weight_file The per-allele SNP effect
 #' @param out Name of output file.
@@ -10,7 +9,6 @@
 #' @return Execute plink. Return 0 if successful.
 #' @export
 compute_PRS = function(
-	plink_exe,
 	geno,
 	weight_file,
 	out,
@@ -21,7 +19,7 @@ compute_PRS = function(
 	download.file("https://s3.amazonaws.com/plink2-assets/alpha3/plink2_linux_x86_64_20221024.zip", "plink.zip")
 	unzip("plink.zip")
 	
-	cmd = system(paste0(plink_exe, " --bfile ", geno, " --score ", weight_file, " cols=+scoresums no-mean-imputation header-read --score-col-nums ", start_col, "-", n, " --out ", out))
+	cmd = system(paste0("plink2 --bfile ", geno, " --score ", weight_file, " cols=+scoresums no-mean-imputation header-read --score-col-nums ", start_col, "-", n, " --out ", out))
 	
 	print(cmd)
 	system(cmd)
