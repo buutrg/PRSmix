d29d77e150b8858bd06e542d1b6c13036f682e5b
Buu Truong <buutrg@gmail.com>
2023-03-27 21:37:13 -0400

    update harmonizing

--- R/harmonize_snpeffect_toALT.R
+++ R/harmonize_snpeffect_toALT.R
@@ -183,7 +183,7 @@ harmonize_snpeffect_toALT = function(
 		relocate(A1, .after=SNP) %>%
 		relocate(A2, .after=A1)
 	writeLines(paste0("Writing output file: ", out))
-	fwrite(res_chunk_all, out, row.names=F, sep=" ", quote=F, na=0)
+	fwrite(res_chunk_all, out, row.names=F, sep=" ", quote=F, na=NA)
 	
 	return(0)
 }
