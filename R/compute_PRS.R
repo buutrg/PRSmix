b2a5b662a1f90ece570ca6798873f8143f8b7132
Buu Truong <buutrg@gmail.com>
2023-04-11 16:01:58 -0400

    add pgen for PRS calc

--- R/compute_PRS.R
+++ R/compute_PRS.R
@@ -12,14 +12,19 @@ compute_PRS = function(
 	geno,
 	weight_file,
 	out,
-	start_col = 4) {
+	start_col = 4,
+	ispgen = F) {
 	
 	n = length(read.table(pipe(paste0("head -n1 ", weight_file)), header=F)[1,])
 	
 	download.file("https://s3.amazonaws.com/plink2-assets/alpha3/plink2_linux_x86_64_20221024.zip", "plink.zip")
 	unzip("plink.zip")
 	
-	cmd = system(paste0("plink2 --bfile ", geno, " --score ", weight_file, " cols=+scoresums no-mean-imputation header-read --score-col-nums ", start_col, "-", n, " --out ", out))
+	if (!ispgen) {
+		cmd = system(paste0("plink2 --bfile ", geno, " --score ", weight_file, " cols=+scoresums no-mean-imputation header-read --score-col-nums ", start_col, "-", n, " --out ", out))
+	} else {
+		cmd = system(paste0("plink2 --pfile ", geno, " --score ", weight_file, " cols=+scoresums no-mean-imputation header-read --score-col-nums ", start_col, "-", n, " --out ", out))
+	}
 	
 	print(cmd)
 	system(cmd)
