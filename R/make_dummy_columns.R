5747bb015fb764a3ccd5263e3cc0c0d1ecafd373
Buu Truong <buutrg@gmail.com>
2023-10-15 22:08:31 -0400

    fix add dummy columns

--- R/make_dummy_columns.R
+++ R/make_dummy_columns.R
@@ -1,10 +1,8 @@
-
-
 #' Make dummy columns for categorical variables
 #'
 #' This function makes dummy columns for categorical variables
 #'
-#' @param filename File name with variables
+#' @param df Dataframe with variables
 #' @param cat_covar_list Categorical variables 
 #' @return This function will generate dummy columns for categorical variables 
 #' - Return data frame with dummy variables of categorical variables 
@@ -13,22 +11,21 @@
 #' @importFrom dplyr bind_rows select all_of mutate group_by summarise rowwise filter
 #' @importFrom fastDummies dummy_cols
 #' @export
-make_dummy_columns = function(filename, cat_covar_list) {
+make_dummy_columns = function(df, cat_covar_list) {
 
     options(datatable.fread.datatable=FALSE)
 
-    dd = fread(filename)
     dummy_colnames = NULL
 
     for (cat_covar_i in 1:length(cat_covar_list)) {
 
         cat_covar = cat_covar_list[cat_covar_i]
-        dd = dd[which(!is.na(dd[,cat_covar])),]
-        dd = dummy_cols(dd, select_columns = cat_covar, remove_selected_columns=T, remove_most_frequent_dummy=T)
-        dummy_colnames_tmp = colnames(dd_dummy)[which(startsWith(colnames(dd_dummy), cat_covar))]
+        df = df[which(!is.na(df[,cat_covar])),]
+        df = dummy_cols(df, select_columns = cat_covar, remove_selected_columns=T, remove_most_frequent_dummy=T)
+        dummy_colnames_tmp = colnames(df)[which(startsWith(colnames(df), cat_covar))]
         dummy_colnames = c(dummy_colnames, dummy_colnames_tmp)
     }
-    return(dd)
+    return(list(df=df, dummy_names=dummy_colnames))
 
 }
 
